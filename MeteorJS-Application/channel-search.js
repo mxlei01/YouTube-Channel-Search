f797d1f982ff357e0f4b9c3fffd687a6d7d237f8
mxlei01 <mxlei01@gmail.com>
2015-09-29 03:32:28 -0600

    Modified the JS and HTML to test getting data from MongoDB

--- MeteorJS-Application/channel-search.js
+++ MeteorJS-Application/channel-search.js
@@ -1,25 +1,37 @@
-// Create Two Mongo Collection: video and user
-// video:
-//      _id = {channelID, videoId}
-//      Title = title
-//      Description = Description
-//      Date = publishedAt
-// user:
-//      _id = {videold, id}
-//      Username = username
-//      textDisplay = textDisplay
-//      Date = updatedAt
-video = new Mongo.Collection("video");
-user = new Mongo.Collection("user");
+// Create Mongo Collection: comments
+// comments:
+//      _id = id (of comments)
+//      Username
+//      textDisplay
+//      Date_Of_Comment
+//      channelID
+//      videold
+//      Title
+//      Description
+//      Date_Of_Video
+comments = new Mongo.Collection("comments");
 
 // Code ran at the server
 if (Meteor.isServer)
 {
-
+    Meteor.publish("comments", function ()
+    {
+        //return comments.find();
+    });
 }
 
 // Code ran at the client
 if (Meteor.isClient)
 {
+    Template.body.helpers
+    (
+        {
+            comments: function ()
+            {
+                return comments.find()
+            }
+        }
+    );
 
+    Meteor.subscribe("comments");
 }
