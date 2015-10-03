68efcb0118fe58e400978e90137e725de3065772
mxlei01 <mxlei01@gmail.com>
2015-10-03 00:30:27 -0600

    Added functionality to show videos of users and their respective comments for each videos

--- MeteorJS-Application/channel-search.js
+++ MeteorJS-Application/channel-search.js
@@ -12,6 +12,18 @@
 //      Date_Of_Video
 comments = new Mongo.Collection("comments");
 
+//Create Mongo Collection: user_video
+// _id:Username
+//	{Video1, channelID}
+//		Comments
+//		Comments
+//	{Video1, channelID}
+//		Comments
+//		Comments
+//	{Video1, channelID}
+//		Comments
+user_videos = new Mongo.Collection("user_video");
+
 // Code ran at the server
 if (Meteor.isServer)
 {
@@ -28,7 +40,7 @@ if (Meteor.isServer)
             // Need to make sure that the channelName is not empty, because it is possible that
             // a collection have some rows that does not have a channel name, and only channel ID
             // and the user selected to search by channel name, but have not typed in any channel name
-            // it will actually accidently search for channel name that are empty, and will return
+            // it will actually accidentally search for channel name that are empty, and will return
             // a lot of data
             if (search != "")
             {
@@ -36,21 +48,44 @@ if (Meteor.isServer)
             }
         }
     });
+    Meteor.publish("user_video", function (channelId)
+    {
+        // Create a search document, and only find the channelID's that exist under every user
+        searchDocument = {};
+        searchDocument[channelId] = { '$exists' : 1 };
+        return user_videos.find(searchDocument);
+    });
 }
 
 // Code ran at the client
 if (Meteor.isClient)
 {
+    // Store subscriptions so that we can stop them later
+    var commentSubscribe = null;
+    var userVideoSubscribe = null;
+
+    // Keep track of the channelId that we retrieve from comments
+    var channelId = "";
+
     Template.body.helpers
     (
         {
-            // We want to return all of the comments we find using the .ui.search value
-            comments: function ()
-            {
-                return comments.find()
-            },
             commentsCount: function()
             {
+                try
+                {
+                    channelId = comments.findOne().channelId;
+                }
+                catch(err){}
+
+                // subscribe to the user_comments where the channelID is what we recieved from any comments
+                // any comments would have the same channelId, and also remove the last subscription
+                try
+                {
+                    userVideoSubscribe.stop();
+                } catch(err) {}
+                userVideoSubscribe = Meteor.subscribe("user_video", channelId);
+
                 // Returns the total count of comments for the channel
                 return comments.find().count()
             },
@@ -71,9 +106,19 @@ if (Meteor.isClient)
                 }).fetch().map(function(x) {
                     return x.videoId;
                 }), true).length;
+            },
+            user_videos: function()
+            {
+                array = [];
+                user_videos.find().forEach(function(users)
+                {
+                    array.push({'_id':users['_id'], 'videos':users[channelId], 'time':users["dateOfReply"]});
+                });
+                return array
             }
         }
     );
+
     // jQuery waits until the documents is fully loaded
     $( document ).ready(function()
     {
@@ -87,8 +132,12 @@ if (Meteor.isClient)
                         beforeSend: function (settings)
                         {
                             // Subscribe to comments collection, and will filter based on the value
-                            // the user typed in the search box
-                            Meteor.subscribe("comments", $('.ui.search').search('get value'), $('.ui.dropdown').dropdown('get value'));
+                            // the user typed in the search box, and also stop the last subscription
+                            try
+                            {
+                                commentSubscribe.stop();
+                            } catch(err) {}
+                            commentSubscribe = Meteor.subscribe("comments", $('.ui.search').search('get value'), $('.ui.dropdown').dropdown('get value'));
 
                             // Changes a header showing the user what was searched
                             $('#userSearch').text($('.ui.search').search('get value'));
@@ -127,6 +176,6 @@ if (Meteor.isClient)
 
         // Initialize a dropdown, so that users can select between Channel ID, and User Name
         // if dropdown is not initialized, it will not work
-        $('.ui.dropdown').dropdown({})
+        $('.ui.dropdown').dropdown({});
     });
 }
