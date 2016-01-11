2e26e80088c05d4370455b625cec027878c4c1c8
Max <mxlei01@gmail.com>
2016-01-11 10:36:37 -0700

    Update search.js

--- MeteorJS-Application/client/jQuery/search.js
+++ MeteorJS-Application/client/jQuery/search.js
@@ -47,7 +47,7 @@ $( document ).ready(function()
                 },
                 // Base URL settings, according to the dropdown, we will either populate a
                 // Channel ID or user name, but not both (limitation of youtube API v3)
-                url: window.location.href+"/channel?";
+                url: window.location.href+"/channel?"
             }
         }
     )
