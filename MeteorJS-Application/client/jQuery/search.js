733cfe4d0db17a7741680b3947c8af90c770497b
Max <mxlei01@gmail.com>
2016-01-11 10:28:33 -0700

    Update search.js

--- MeteorJS-Application/client/jQuery/search.js
+++ MeteorJS-Application/client/jQuery/search.js
@@ -47,7 +47,7 @@ $( document ).ready(function()
                 },
                 // Base URL settings, according to the dropdown, we will either populate a
                 // Channel ID or user name, but not both (limitation of youtube API v3)
-                url: 'http://mxlei01.com:8888/channel?'
+                url: window.location.href+"/channel?";
             }
         }
     )
